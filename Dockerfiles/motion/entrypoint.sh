dce1494e031afe7083d89651c8e654f0cbd90ea1
granthouston44 <grant.houston44@hotmail.com>
2024-01-04 20:07:08 +0000

    updating entrypoint script to ensure noip client is running

--- Dockerfiles/motion/entrypoint.sh
+++ Dockerfiles/motion/entrypoint.sh
@@ -4,6 +4,15 @@
 cd /usr/local/src/noip
 ./configure-noip.expect "$NOIP_USERNAME" "$NOIP_PASSWORD"
 
+# Check if noip2 is running and start it if not
+if ! pgrep -x "noip2" > /dev/null
+then
+    echo "Starting No-IP Client..."
+    noip2
+else
+    echo "No-IP Client is already running."
+fi
+
 # Configure Motion with authentication credentials
 sed -i "s/stream_authentication username:password/stream_authentication $MOTION_USERNAME:$MOTION_PASSWORD/g" /etc/motion/motion.conf
 
